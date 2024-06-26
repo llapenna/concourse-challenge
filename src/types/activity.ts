d0e2a0732bb318b48ee6ff3034377f1ba7840e2f
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-06-25 23:43:59 -0300

    feat(services): add endpoint and type the API response

---
+++ src/types/activity.ts
@@ -0,0 +1,8 @@
+/**
+ * Activity data right from the API
+ */
+export interface RawActivity {
+  days: number[];
+  total: number;
+  week: number;
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Github API URL
+ */
+export const COMMITS_API =
+  "https://api.github.com/repos/facebook/react/stats/commit_activity";
