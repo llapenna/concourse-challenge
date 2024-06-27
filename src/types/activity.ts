e3e7d002280442d3daf95345c8844dc15fc4a13a
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-06-27 16:56:30 -0300

    feat(types): type each commit intensity level

--- src/types/activity.ts
+++ src/types/activity.ts
@@ -6,3 +6,14 @@ export interface RawActivity {
   total: number;
   week: number;
 }
+
+/**
+ * Scale information used to represent each day's intensity
+ */
+export enum Intensity {
+  Empty = 0,
+  Low = 1,
+  Medium = 2,
+  High = 3,
+  Full = 4,
+}
