938cb659f17533ad159cea35a4cd31fa50b895e9
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-06-27 13:48:43 -0300

    feat: add week days and months constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,34 @@
  */
 export const COMMITS_API =
   "https://api.github.com/repos/facebook/react/stats/commit_activity";
+
+/**
+ * Days of the week used to render the graph
+ */
+export const WEEK_DAYS = [
+  { expanded: "Sunday", short: "Sun" },
+  { expanded: "Monday", short: "Mon" },
+  { expanded: "Tuesday", short: "Tue" },
+  { expanded: "Wednesday", short: "Wed" },
+  { expanded: "Thursday", short: "Thu" },
+  { expanded: "Friday", short: "Fri" },
+  { expanded: "Saturday", short: "Sat" },
+];
+
+/**
+ * Each month of the year used to render the graph
+ */
+export const MONTHS = [
+  { expanded: "January", short: "Jan" },
+  { expanded: "February", short: "Feb" },
+  { expanded: "March", short: "Mar" },
+  { expanded: "April", short: "Apr" },
+  { expanded: "May", short: "May" },
+  { expanded: "June", short: "Jun" },
+  { expanded: "July", short: "Jul" },
+  { expanded: "August", short: "Aug" },
+  { expanded: "September", short: "Sep" },
+  { expanded: "October", short: "Oct" },
+  { expanded: "November", short: "Nov" },
+  { expanded: "December", short: "Dec" },
+];
