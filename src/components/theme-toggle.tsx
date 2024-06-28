ab525f01e6a8efab2a60b0c0665f22be9dbab9f3
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-06-28 02:40:42 -0300

    feat(layout): improve theme toggler by using react-toggle lib

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,7 +1,25 @@
+import { Toggle } from "react-toggle-component";
+import { Moon, SunDim } from "phosphor-react";
+
 import { useTheme } from "@/hooks/useTheme";
+import { hstack } from "@/panda/patterns";
 
 export const ThemeToggle = () => {
   const { toggle } = useTheme();
-  // TODO: add styles/animations
-  return <button onClick={toggle}>toggle</button>;
+  return (
+    <label htmlFor="theme-toggle" className={hstack({ cursor: "pointer" })}>
+      <SunDim size={24}></SunDim>
+      <Toggle
+        onToggle={toggle}
+        name="theme-toggle"
+        leftBackgroundColor="#ecfdf5"
+        leftKnobColor="#070F2B"
+        leftBorderColor="#070F2B"
+        rightBackgroundColor="#070F2B"
+        rightKnobColor="#ecfdf5"
+        rightBorderColor="#ecfdf5"
+      ></Toggle>
+      <Moon size={24}></Moon>
+    </label>
+  );
 };
